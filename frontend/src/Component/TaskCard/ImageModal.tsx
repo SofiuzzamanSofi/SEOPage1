8cb5652257e55a4d0b0e638407d0fcd07de062e3
Sofiuzzaman Sofi <sofiuzzaman.sf@gmail.com>
2024-01-13 15:01:02 +0600

    image upload ok-

--- frontend/src/Component/TaskCard/ImageModal.tsx
+++ frontend/src/Component/TaskCard/ImageModal.tsx
@@ -7,28 +7,25 @@ interface ImageModalProps {
 
 const ImageModal: FC<ImageModalProps> = ({ modalOpenClosed }) => {
 
-    const handleImageChange = async (event: ChangeEvent<HTMLInputElement>) => {
-        const files = event.target.files;
-        // const file = event.target.files?.[0];
+    const handleFileChange = async (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        const imageData = new FormData();
+        imageData.set("key", import.meta.env.VITE_IMAGEBB_KEY);
+        imageData.append("image", event.target.files![0]);
 
-        if (files?.length) {
-            const imageData = new FormData();
-            imageData.set("key", import.meta.env.VITE_IMAGEBB_KEY);
-
-            for (let i = 0; i < files.length; i++) {
-                imageData.append(`image${i}`, files[i]);
-            }
-
-            try {
-                const response = await axios.post(import.meta.env.VITE_IMAGEBB_URL, imageData);
-                const imageUrls = response?.data?.data;
-                console.log('Image URLs:', imageUrls);
-            } catch (error) {
-                console.error('Image upload failed:', error);
-                // Handle the error appropriately
-            }
+        try {
+            const response = await axios.post(
+                "https://api.imgbb.com/1/upload",
+                imageData
+            );
+            const imageUrl = response?.data?.data?.display_url;
+            console.log('imageUrl:', imageUrl);
+        } catch (error) {
+            console.log('error:', error);
         }
     };
+
 
     return (
         <div className={`${modalOpenClosed ? "hidden" : ""} min-h-screen w-full bg-[#eeeeee] absolute  top-[-10px] bottom-[-10px] left-0 right-0 flex justify-center items-center z-10`}>
@@ -38,10 +35,10 @@ const ImageModal: FC<ImageModalProps> = ({ modalOpenClosed }) => {
                         style={{ zIndex: 999, opacity: 0, width: '320px', height: '200px', position: 'absolute', right: '0px', left: '0px', marginRight: 'auto', marginLeft: 'auto' }}
                         name="files[]"
                         id="filer_input2"
-                        multiple
+                        multiple={false}
                         type="file"
                         className='cursor-pointer'
-                        onChange={(event) => handleImageChange(event)}
+                        onChange={(event) => handleFileChange(event)}
                     />
                     <div className="border mt-4 p-4 text-center text-gray-700 bg-white border-dashed border-gray-300 transition duration-300">
                         <div className="">
